0ad363206f6adc627ecd8ef8dee8cf56c6f3db2f
yura123123 <yurachorniy123@gmail.com>
2021-04-25 15:06:58 +0300

    Adding project to repository

---
+++ Assets/Scripts/Behaviour/LivingEntity.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class LivingEntity : MonoBehaviour {
+
+    public int colourMaterialIndex;
+    public Species species;
+    public Material material;
+
+    public Coord coord;
+    //
+    [HideInInspector]
+    public int mapIndex;
+    [HideInInspector]
+    public Coord mapCoord;
+
+    protected bool dead;
+
+    public virtual void Init (Coord coord) {
+        this.coord = coord;
+        transform.position = Environment.tileCentres[coord.x, coord.y];
+
+        // Set material to the instance material
+        var meshRenderer = transform.GetComponentInChildren<MeshRenderer> ();
+        for (int i = 0; i < meshRenderer.sharedMaterials.Length; i++)
+        {
+            /*if (material == null)
+            {
+                material = meshRenderer.materials[0];
+            }*/
+            /*if (meshRenderer.sharedMaterials[i] == material)
+            {
+                material = meshRenderer.materials[i];
+                break;
+            }*/
+
+            if (meshRenderer.sharedMaterials[i] is Material)
+            {
+                material = meshRenderer.materials[i];
+                break;
+            }
+        }
+    }
+
+    protected virtual void Die (CauseOfDeath cause) {
+        if (!dead) {
+            dead = true;
+            Environment.RegisterDeath (this);
+            Destroy (gameObject);
+        }
+    }
+}
---
+++ Ecosystem-2-master/Assets/Scripts/Behaviour/Rabbit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rabbit : Animal {
+    public static readonly string[] GeneNames = { "A", "B" };
+
+    float amountRemaining = 1;
+    const float consumeSpeed = 8;
+
+    public float AmountRemaining
+    {
+        get
+        {
+            return amountRemaining;
+        }
+    }
+}
